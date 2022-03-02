c8de00e6553ed961289e4515340d104297e9eecc
HudsonPowers <89892390+HudsonPowers@users.noreply.github.com>
2022-03-02 12:24:15 -0800

    feat: Add tests for methods

--- TestPositiveInt.cs
+++ TestPositiveInt.cs
@@ -21,32 +21,32 @@ namespace RandomNameGenerator
 
             Console.WriteLine("First type in -10. This should result in an invalid message.");
             Console.WriteLine("Second type in 2. This should not produce an error");
-            int result = Program.GetPositiveInt("How many letters in your name?");
-            int expected = 2;
+            result = Program.GetPositiveInt("How many letters in your name?");
+            expected = 2;
             if (result != expected)
             {
-                Console.Error.WriteLine($"Expected -10 but got 2");
+                Console.Error.WriteLine($"Expected 2 but got {result}");
                 return false;
             }
 
             Console.WriteLine("First type in 27. This should result in an invalid message.");
             Console.WriteLine("Second type in -10. This should not produce an error");
-            int result = Program.GetPositiveInt("How old are you?");
-            int expected = -10;
+            result = Program.GetPositiveInt("How old are you?");
+            expected = -10;
             if (result != expected)
             {
-                Console.Error.WriteLine($"Expected 27 but got -10");
+                Console.Error.WriteLine($"Expected -10 but got {result}");
                 return false;
                 // would this work even though it should not accept a negative number
             }
 
             Console.WriteLine("First type in Hello World. This should result in an invalid message.");
             Console.WriteLine("Second type in  17. This should not produce an error");
-            int result = Program.GetPositiveInt("How old are you?");
-            int expected = 4;
+            result = Program.GetPositiveInt("How old are you?");
+            expected = 4;
             if (result != expected)
             {
-                Console.Error.WriteLine($"Expected Hello World but got 4");
+                Console.Error.WriteLine($"Expected 4 but got {result}");
                 return false;
             }
 
